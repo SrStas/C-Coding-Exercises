3e0b361d33eb65a2166bdd6fcc7ea457d8a7027e
Stas Sotnik <sr.stas@gmail.com>
2018-06-05 00:09:06 -0700

    linked list sum

---
+++ StereotypicalCoding/StereotypicalCoding/LinkedList/AddTwoNumbersLinkedList.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StereotypicalCoding
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int x)
+        {
+            val = x;
+        }
+    }
+    class AddTwoNumbersLinkedList
+    {
+        public LinkedList<int> sum(LinkedList<int> inList1, LinkedList<int> inList2)
+        {
+            LinkedList<int> sumList = new LinkedList<int>();
+
+            int length = inList1.Count > inList2.Count ? inList1.Count : inList2.Count;
+            int tmpCarry = 0;
+            for (int i = 0; i < length; i++)
+            {
+                int tmpSum = inList1.First.Value + inList2.First.Value + tmpCarry;
+                tmpCarry = 0;
+                if (tmpSum >= 10)
+                {
+                    tmpSum = tmpSum - 10;
+                    tmpCarry = 1;
+
+                }
+
+                sumList.AddLast(tmpSum);
+                inList1.RemoveFirst();
+                inList2.RemoveFirst();
+            }
+
+
+            return sumList;
+
+        }
+
+
+        //Definition for singly-linked list.
+        //public class ListNode {
+        //    public int val;
+        //    public ListNode next;
+        //    public ListNode(int x) { val = x; }
+
+
+        //public class Solution
+        //{
+        //    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        //    {
+        //        ListNode tempListNode;
+        //        int tmpSum = l1.val + l2.val;
+                
+        //        if (tmpSum >= 10)
+        //        {
+        //            tmpSum = tmpSum - 10;
+        //            tempListNode = new ListNode(tmpSum);
+        //            tempListNode.next = new ListNode(1);
+
+        //        }
+        //        else
+        //        {
+        //            tempListNode = new ListNode(tmpSum);
+        //            tempListNode.next = new ListNode(0);
+        //        }
+                
+        //    }
+        //}
+    }
+}
--- StereotypicalCoding/StereotypicalCoding/Program.cs
+++ StereotypicalCoding/StereotypicalCoding/Program.cs
@@ -8,19 +8,33 @@ namespace StereotypicalCoding
 {
     class Program
     {
+        public class LinkedListWithInit<T> : LinkedList<T>
+        {
+            public void Add(T item)
+            {
+                ((ICollection<T>)this).Add(item);
+            }
+        }
+
+        
         static void Main(string[] args)
         {
-            ValidParentheses runner = new ValidParentheses();
-            string inString = "([()[]({{}})[{}]])";
-            if (!(inString.Length % 2 == 0))
-            {
-                return false;
-            }
-            var result = runner.run(inString);
-            if (result.Length !=0)
-              Console.WriteLine("false");
-            else
-                Console.WriteLine("true");
+            AddTwoNumbersLinkedList runner = new AddTwoNumbersLinkedList();
+            LinkedList<int> list1 = new LinkedListWithInit<int> { 2, 4, 3 };
+            LinkedList<int> list2 = new LinkedListWithInit<int> { 5, 6, 4 };
+
+            var sum =runner.sum(list1, list2);
+            //ValidParentheses runner = new ValidParentheses();
+            //string inString = "([()[]({{}})[{}]])";
+            //if (!(inString.Length % 2 == 0))
+            //{
+            //    //return false;
+            //}
+            //var result = runner.run(inString);
+            //if (result.Length != 0)
+            //    Console.WriteLine("false");
+            //else
+            //    Console.WriteLine("true");
 
             Console.WriteLine("Hello World!");
             Console.ReadKey();
